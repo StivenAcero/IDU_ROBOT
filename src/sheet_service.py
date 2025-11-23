591e016c5215aae281a815db9eb30ff38caefae2
Stiven Acero <bstivenacero@ucundinamarca.edu.co>
2025-11-23 18:04:38 -0500

    sheet_service update file

--- src/sheet_service.py
+++ src/sheet_service.py
@@ -1,7 +1,7 @@
 import logging
 from typing import List
 import pandas as pd
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 from src.google_credentials import GoogleCredentials
 from src.idu_config import IduConfig
 
@@ -13,27 +13,35 @@ class SheetService:
     def __init__(self):
         self._config = IduConfig.load_config('config/config.json')
         self._creds = GoogleCredentials.get_credentials(self._config.scopes, root_path='config/')
-        self._service = build('sheets', 'v4', credentials=self._creds)
-        self._logger = logging.getLogger(__name__)        
+        self._service: Resource = build('sheets', 'v4', credentials=self._creds)
+        self._logger = logging.getLogger(__name__)
+        # El método spreadsheets() se agrega dinámicamente a la insancia de _service
+        # pylint: disable=maybe-no-member
+        self._spreadsheets = self._service.spreadsheets() # pyright: ignore[reportAttributeAccessIssue]
 
-    def read_sheet(self, spreadsheet_id, range_name):
+    def read_sheet(self) -> List:
         try:
-            sheet = self._service.spreadsheets()
-            result = sheet.values().get(
-                spreadsheetId=spreadsheet_id,
-                range=range_name
-            ).execute()
-            
+            # El método spreadsheets() se agrega dinámicamente a la insancia de servicio
+            # pylint: disable=maybe-no-member
+            result = (
+                self._spreadsheets.values()
+                .get(
+                    spreadsheetId=self._config.spreadsheet_id,
+                    range=self._config.range_name,
+                )
+                .execute()
+            )
+
             values = result.get('values', [])
             return values
         except Exception as e:
             self._logger.error("Error al leer la hoja:  %s", e,  exc_info=True)
-            return None
+            return []
 
     def validate_missing_files(self, data: List) -> tuple[int, List[dict], int]:
         if not data or len(data) < 2:
             return 0, [], 0
-        
+
         # Obtener índices de columnas
         try:
             header = data[0]
@@ -42,15 +50,14 @@ class SheetService:
         except ValueError :
             self._logger.error("Error: No se encontró la columna requerida. %s")
             return 0, [], 0
-        
+
         registros_sin_estado = [
             self._crear_registro_sin_estado(fila, indice_chips, indice_estado)  # Sin 'i'
             for i, fila in enumerate(data[1:], start=2)
             if self._estado_vacio(fila, indice_estado)
         ]
-        
-        return len(registros_sin_estado), registros_sin_estado, len(data) - 1
 
+        return len(registros_sin_estado), registros_sin_estado, len(data) - 1
 
     def _estado_vacio(self, fila: List, indice_estado: int) -> bool:
         """Verifica si el estado está vacío o no existe."""
@@ -59,11 +66,9 @@ class SheetService:
         estado = fila[indice_estado].strip() if fila[indice_estado] else ''
         return not estado
 
-
     def _obtener_chips(self, fila: List, indice_chips: int) -> str:
-        
-        return fila[indice_chips] if len(fila) > indice_chips else ''
 
+        return fila[indice_chips] if len(fila) > indice_chips else ''
 
     def _crear_registro_sin_estado(self, fila: List, numero_fila: int, indice_chips: int,) -> dict:
         return {
@@ -76,21 +81,21 @@ class SheetService:
 
         if not registros_sin_estado:
             return pd.DataFrame(columns=['solicitud', 'chips'])
-        
+
         solicitudes = []
-        
+
         # Dividir en grupos de máximo 5
         for i in range(0, len(registros_sin_estado), max_por_grupo):
             grupo_actual = registros_sin_estado[i:i + max_por_grupo]
-            
+
             # Extraer solo los CHIPS
             chips_grupo = [reg['CHIPS'] for reg in grupo_actual]
-            
+
             solicitudes.append({
                 'solicitud': len(solicitudes) + 1,
                 'chips': chips_grupo
             })
-                
+
         return pd.DataFrame(solicitudes)
 
     def obtener_letra_columna(self, encabezado, nombre_columna) -> str:
@@ -112,12 +117,8 @@ class SheetService:
         except ValueError:
             self._logger.error("No se encontró la columna CHIPS")
             return 0
-        range_name = 'A:Z'
-        result = self._service.spreadsheets().values().get(
-            spreadsheetId=spreadsheet_id,
-            range=range_name
-        ).execute()
-        data = result.get('values', [])
+
+        data = self.read_sheet()
         batch_data = []
         chips_actualizados = []
         for chip in chips_list:
@@ -136,7 +137,7 @@ class SheetService:
                     'valueInputOption': 'RAW',
                     'data': batch_data
                 }
-                result = self._service.spreadsheets().values().batchUpdate(
+                self._spreadsheets.values().batchUpdate(
                     spreadsheetId=spreadsheet_id,
                     body=body
                 ).execute()
